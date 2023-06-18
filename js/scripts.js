9aab04dfec00a507ba4b2db9ae4fdfd39e6fa4c3
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-06-18 18:49:08 +0200

    feat: add new feature to add a new list with a test web storage feature, change responsiveness, update description, add modal instead of alert, style the modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,33 @@
+$('body').on('load', checkEdits());
+function checkEdits() {
 
+  //find out if the user has previously saved edits
+  if(localStorage.userEdits!=null)
+  $(".list-title").html(localStorage.userEdits);
+  }
+  // $( function() {
+  //   $(document).tooltip();
+  // } );
+  // $( function() {
+  //   $( document ).tooltip();
+  // } );
+  
 function newItem(){
-
+  
     //javascript
     //1. Adding a new item to the list of items: 
        let li = $("<li></li>");
        let inputValue = $("#input").val();
+      //  $('#input').on('hover', tooltip());
        li.append(inputValue);
        
     
        if (inputValue === '') {
-         alert("Please enter your list item!!");
+         $('#dialog').dialog();
        } else {
-         $('#list').append(li);
+        $('#list').append(li);
+        // $('.dropped-to-div').append('#list')
+         
        }
     
      //2. Crossing out an item from the list of items:
@@ -41,7 +57,72 @@ function newItem(){
        $('#list').sortable(); 
     }
     
+    function addList(){
+      
+    //javascript
+    //1. Adding a new item to the list of items: 
+       let div = $("<div class='new-list'></div>");
+       $('.container').append(div);
+       let title = $("<h4 class='list-title'></h4>");
+       div.append(title);
+       let updateEdits = $('<p class="update">Edit the title and click to save for the next time</p>');
+       div.append(updateEdits); 
+      //  let counter = 1;
+       
+      //  let inputValue = $("#input").val();
+       title.text('New List');
+       title.attr('contenteditable',"true");
+       let saveEdits = $('<input type="button" class="save-edits" value="save my edits" title="Click to save your edits" onclick="saveEdits()"/>');
+       div.append(saveEdits);
+       
+       
+         
+    
+      //  if (inputValue === '') {
+      //    alert("Please enter your list item!!");
+      //  } else {
+      //    $('#list').append(li);
+      //  }
+      
+     //2. Crossing out an item from the list of items:
+      //  function crossOut() {
+      //        title.toggleClass("strike");
+      //    }
 
+
+        //  li.on('doubletap', crossOut);
+
+        //  div.on("dblclick", crossOut);
+        //  div.on("touchend", crossOut);
+         
+        //  li.on('doubletap', crossOut);
+      
+     //3(i). Adding the delete button "X": 
+       let crossOutButton = $("<crossOutButton></crossOutButton>");
+         crossOutButton.append(document.createTextNode("X"));
+         div.append(crossOutButton);
+    
+         crossOutButton.on("click", deleteList);
+     //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+       function deleteList(){
+             div.addClass("delete")
+         }
+     // 4. Reordering the items: 
+      //  $('#list').draggable().sortable(); 
+    }
+
+    function saveEdits() {
+
+      //get the edited element content
+      var userVersion = $('.list-title').html();
+      
+      //save the content to local storage
+      localStorage.userEdits = userVersion;
+      
+      //write a confirmation to the user
+      $(".update").text("Edits saved!");
+      
+      }
     //Test function for adding 
     // a list item when 
     // pressing enter
